256b300817bdab7e5aaa1b0179d4afb014fa55b4
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2022-06-02 09:49:07 +0500

    suspens)))

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
@@ -17,21 +17,24 @@ const HomePage = () => {
                                 </div>
 
                         </Fader>
-                        <Fader delay={450}>
-                                <div className="carousel__wrapper">
-                                        <Carousel className="main-slide" autoPlay={true} interval={2000} infiniteLoop={true}>
-                                                <div>
-                                                        <img src="/images/Asian.svg" alt="" height='300px' width='200px' />
-                                                </div>
-                                                <div>
-                                                        <img src="/images/Sad.svg" alt="" height='300px' width='200px'/>
-                                                </div>
-                                                <div>
-                                                        <img src="/images/Naverh.svg" alt="" height='300px' width='200px'/>
-                                                </div>
-                                        </Carousel>
-                                </div>
-                        </Fader>
+                        <Suspense fallback={<div>Loading...</div>}>
+                                <Fader delay={450}>
+                                        <div className="carousel__wrapper">
+                                                <Carousel className="main-slide" autoPlay={true} interval={2000} infiniteLoop={true}>
+                                                        <div>
+                                                                <img src="/images/Asian.svg" alt="" height='300px' width='200px' />
+                                                        </div>
+                                                        <div>
+                                                                <img src="/images/Sad.svg" alt="" height='300px' width='200px'/>
+                                                        </div>
+                                                        <div>
+                                                                <img src="/images/Naverh.svg" alt="" height='300px' width='200px'/>
+                                                        </div>
+                                                </Carousel>
+                                        </div>
+                                </Fader>
+                        </Suspense>
+                        
                 </>
         );
 };
