82554476f908287848c6ab36084ae533c34b873e
ViaChessLove <85558513+ViaChessLove@users.noreply.github.com>
2022-06-21 18:16:35 +0500

    q

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React, { Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 
+const LazyApp = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Suspense fallback={<div>Loading...</div>}>
-    <App />
+    <LazyApp />
   </Suspense>
 
 );
